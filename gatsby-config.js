2209e9165f2febb69775ed65505174f801a9cc73
lmeikle <laura.s.meikle@gmail.com>
2019-02-09 17:00:32 +0000

    pwa and deploy to github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Learn some simple greek words`,
     author: `@lmeikle`,
   },
+  pathPrefix: '/learn-greek',
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
@@ -19,18 +20,18 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Learn Greek`,
+        short_name: `Greek`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#296997`,
+        theme_color: `#296997`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/greek-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    'gatsby-plugin-offline',
     `gatsby-transformer-json`,
     {
       resolve: `gatsby-source-filesystem`,
